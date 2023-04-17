ce9c4f5a9d865d39e389a8f1e1e1fba764e5411a
Otávio Gabriel <tavieto@outlook.com>
2023-04-17 17:11:11 -0300

    Initial Commit

---
+++ I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Otávio Gabriel Silva Moreira on 17/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
